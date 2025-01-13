42e39117f80673e266a98ac88cb4dbd38313813d
antonioferrigno28 <ferrigno1@hotmail.it>
2025-01-13 17:13:06 +0100

    Working routes & layout

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,7 +2,7 @@ export default function AboutPage() {
   return (
     <>
       <div className="container py-4">
-        <h1> about</h1>;
+        <h1>About</h1>
       </div>
     </>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <>
       <div className="container py-4">
-        <h1> Homepage</h1>;
+        <h1>Homepage</h1>
       </div>
     </>
   );
